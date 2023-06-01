35750d0cba89eac7a355032cc55c422cd6805d0b
Adam Grubb <adam.grubb.63@hotmail.com>
2023-06-01 12:45:13 +0200

    Create right order for MVC

--- RefactoringExerciseVS/Program.cs
+++ RefactoringExerciseVS/Program.cs
@@ -1,8 +1,11 @@
 ﻿using RefactoringExerciseVS.Controller;
+using RefactoringExerciseVS.Model;
 using RefactoringExerciseVS.View;
 using System.Text;
 
-CalculatorController controller = new CalculatorController();
-ConsoleCalculator view = new ConsoleCalculator(controller);
-view.Start();
+ConsoleCalculator consoleCalculator = new ConsoleCalculator();
+DoubleStack doubleStack = new DoubleStack();
+NormalStack normalStack = new NormalStack();
+CalculatorController calculatorController = new CalculatorController(consoleCalculator, doubleStack);
+calculatorController.Start();
 
