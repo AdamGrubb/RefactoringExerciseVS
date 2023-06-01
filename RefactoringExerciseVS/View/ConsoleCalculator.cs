df6f110926ccc3c62518fc5ff3c9aa5a978615f3
Adam Grubb <adam.grubb.63@hotmail.com>
2023-06-01 12:44:13 +0200

    Create a proper ConsoleViewClass and interface

--- RefactoringExerciseVS/View/ConsoleCalculator.cs
+++ RefactoringExerciseVS/View/ConsoleCalculator.cs
@@ -8,37 +8,8 @@ using System.Threading.Tasks;
 
 namespace RefactoringExerciseVS.View
 {
-    public class ConsoleCalculator : IConsoleCalculator
+    public class ConsoleCalculator : ICalculatorView
     {
-        private readonly ICalculatorController calculatorController;
-
-        public ConsoleCalculator(ICalculatorController calculatorController)
-        {
-            this.calculatorController = calculatorController;
-        }
-        public void Start()
-        {
-            while (true)
-            {
-                if (this.calculatorController.GetStackCount() == 0)
-                {
-                    Output("Commands: q c + - * / number");
-                    Output("[]");
-                }
-                else
-                {
-                    Output(printStack());
-                }
-                var success = calculatorController.UserInput(Input());
-                if (!success)
-                {
-                    Console.WriteLine("Invalid input");
-                }
-
-            }
-
-
-        }
         public string Input()
         {
             return Console.ReadLine();
@@ -46,19 +17,6 @@ namespace RefactoringExerciseVS.View
         public void Output(string outPut)
         {
             Console.WriteLine(outPut);
-        }
-        private string printStack() //Works, but need refactor.
-        {
-            var stackStack = calculatorController.GetStack().ToList();
-            StringBuilder stackToString = new StringBuilder();
-            stackToString.Append('[');
-            for (int i = 0; i < stackStack.Count; i++)
-            {
-                stackToString.Append(stackStack[i]);
-                if (i != stackStack.Count - 1) stackToString.Append(", ");
-            }
-            stackToString.Append(']');
-            return stackToString.ToString();
         }
     }
 
