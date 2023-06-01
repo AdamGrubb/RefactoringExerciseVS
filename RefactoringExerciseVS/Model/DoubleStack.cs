707deef045c89a4d490f65d8652bcec864e0380a
Adam Grubb <adam.grubb.63@hotmail.com>
2023-06-01 12:43:16 +0200

    Create a DoubleStackClass and NormalStackClass

---
+++ RefactoringExerciseVS/Model/DoubleStack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactoringExerciseVS.Model
+{
+    class DoubleStack : IStack
+    {
+        private double[] data;
+        private int Depth;
+
+        public DoubleStack()
+        {
+            data = new double[1000];
+            Depth = 0;
+        }
+
+        public void Push(double value)
+        {
+            data[Depth++] = value;
+        }
+
+        public double Pop()
+        {
+            return data[--Depth];
+        }
+        public void Clear()
+        {
+            Depth = 0;
+        }
+        public int Count()
+        {
+            return Depth;
+        }
+
+        public double[] ListData()
+        {
+            return data;
+        }
+    }
+}
---
+++ RefactoringExerciseVS/Model/NormalStack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace RefactoringExerciseVS.Model
+{
+    public class NormalStack : IStack
+    {
+        private Stack<double> stack = new Stack<double>();
+        public void Clear()
+        {
+            stack.Clear();
+        }
+
+        public int Count()
+        {
+            return stack.Count();
+        }
+
+        public double Pop()
+        {
+            return stack.Pop();
+        }
+
+        public double[] ListData()
+        {
+            return stack.Reverse().ToArray();
+        }
+
+        public void Push(double value)
+        {
+            stack.Push(value);
+        }
+    }
+}
