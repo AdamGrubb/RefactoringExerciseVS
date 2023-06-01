53c43a198d01f472ee2676030b9dbd6092b729ac
Adam Grubb <adam.grubb.63@hotmail.com>
2023-06-01 10:29:19 +0200

    Remove view-logic from Program.cs

--- RefactoringExerciseVS/Program.cs
+++ RefactoringExerciseVS/Program.cs
@@ -1,38 +1,10 @@
 ﻿using RefactoringExerciseVS.Controller;
 using RefactoringExerciseVS.Model;
+using RefactoringExerciseVS.View;
 using System.Text;
 
-Calculator calculator = new Calculator();
+StackCalculator calculator = new StackCalculator();
 CalculatorController controller = new CalculatorController(calculator);
+ConsoleCalculator view = new ConsoleCalculator(controller);
+view.Start();
 
-while (true)
-{
-    if (controller.GetStackCount() == 0)
-    {
-        Console.WriteLine("Commands: q c + - * / number");
-        Console.WriteLine("[]");
-    }
-    else
-    {
-        Console.WriteLine(printStack());
-    }
-    var success = controller.UserInput(Console.ReadLine());
-    if (!success)
-    {
-        Console.WriteLine("Invalid input");
-    }
-    
-}
-string printStack() //Works, but need refactor.
-{
-    var stackStack = calculator.stack.ToList();
-    StringBuilder stackToString = new StringBuilder();
-    stackToString.Append('[');
-    for (int i = 0; i < stackStack.Count; i++)
-    {
-        stackToString.Append(stackStack[i]);
-        if (i != stackStack.Count - 1) stackToString.Append(", ");
-    }
-    stackToString.Append(']');
-    return stackToString.ToString();
-}
